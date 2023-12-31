aca0ba4f674bd5d7b140c3d93ca9b20ce9eee256
Surendhar-V <v.surendharsuren222@gmail.com>
2023-12-31 21:15:50 +0530

    Comit

--- src/dummy.java
+++ src/dummy.java
@@ -1,26 +1,101 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class dummy{
-    public static void main(String[] args) {
 
-    Set<String> set = new HashSet<>();
+    public static void main(String[] args){
 
-    String s = "11001 11010 11100 11000 11110 11101 10010 11011 101010 101011 11111 10110 10111 10011 0000 10000 10001 0011 0110 0100 0101 0010 0001 01110 10100 01111";
-    s.trim();
-    String[] arr = s.split(" ");
+      node obj1 = new node(1);
+      obj1.right = new node(2);
+      obj1.left = new node(3);
 
-    for(int i = 0 ; i<arr.length ; i++){
+      obj1.right.right = new node(4);
+      obj1.right.left = new node(5);
 
-            set.add(arr[i]);
+      obj1.left.right = new node(6);
+      obj1.left.left = new node(7);
+
+      insert(obj1 , 10);
+      insert(obj1 , 11);
+      preOrder(obj1);
 
     }
-        System.out.println(set.size());
+
+    public static void preOrder(node node){
+
+        if(node == null){
+            return;
+        }
+
+        System.out.println(node.value);
+
+        preOrder(node.left);
+        preOrder(node.right);
 
 
     }
 
+    public static void inOrder(node node){
 
+        if(node == null){
+            return;
+        }
+
+        inOrder(node.left);
+
+        System.out.println(node.value);
+
+        inOrder(node.right);
+
+    }
+
+    public static void postOrder(node node){
+
+    if(node == null){
+        return;
+    }
+
+    postOrder(node.left);
+    postOrder(node.right);
+
+
+    System.out.println(node.value);
+
+    }
+
+    public static boolean insert(node node , int value){
+
+        if(node.left==null){
+            node.left = new node(value);
+            return true;
+        }else if(node.right == null){
+            node.right = new node(value);
+            return true;
+        }
+
+        if(insert(node.left , value)){
+          return true;
+        }
+
+        if(insert(node.right , value)){
+            return true;
+        }
+
+        return false;
+
+    }
+
+    static class node {
+
+        node right;
+        node left;
+        int value;
+
+        node(int value){
+            this.value = value;
+        }
+
+
+
+
+    }
 
 
 
